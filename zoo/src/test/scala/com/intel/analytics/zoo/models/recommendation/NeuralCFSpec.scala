b58f37785c9709bfea27d4ef3ea531a7e67fc717
cyita <yitastudy@gmail.com>
2020-04-07 09:43:27 +0800

    tensor support

--- zoo/src/test/scala/com/intel/analytics/zoo/models/recommendation/NeuralCFSpec.scala
+++ zoo/src/test/scala/com/intel/analytics/zoo/models/recommendation/NeuralCFSpec.scala
@@ -35,6 +35,8 @@ import org.apache.log4j.{Level, Logger}
 import org.apache.spark.{SparkConf, SparkContext}
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.{DataFrame, SQLContext}
+import com.intel.analytics.bigdl.numeric.NumericFloat
+import com.intel.analytics.zoo.pipeline.inference.InferenceModel
 
 
 class NeuralCFSpec extends ZooSpecHelper {
@@ -227,6 +229,13 @@ class NeuralCFSpec extends ZooSpecHelper {
   }
 
 
+  "NeuralCF" should "inference Inference model" in {
+    val model = new InferenceModel(1)
+    model.doLoadBigDL("/home/yina/Documents/model/ncf.model")
+    val userItemInput = Tensor(T(T(1.0f, 1193.0f), T(1.0f, 661.0f)))
+    val result = model.doPredict(userItemInput)
+    print("load success")
+  }
 }
 
 class NeuralCFSerialTest extends ModuleSerializationTest {
